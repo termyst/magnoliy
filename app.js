5f9940f2676870a0c2bcebf900e6a8f5fd59641b
termyst <131294147+termyst@users.noreply.github.com>
2023-06-28 19:45:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+$(document).ready(function () {
+
+	$('.nav a[href^="#"]').click(function () {
+		//Сохраняем значение атрибута href в переменной:
+		var offset = $('.nav').innerHeight();
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top //можно вычесть высоту меню
+		}, 500);
+		$('.nav a[href^="#"]').removeClass('active');
+		$(this).addClass('active');
+		return false;
+	});
+
+});
+
+
 //ServicesSlider
 
 let slider = $("#slider");
